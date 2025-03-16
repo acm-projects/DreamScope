8dcd16ed44ffb473be2b7d2f45b1984856e2cd9c
BradKnock <aproteem21@gmail.com>
2025-03-15 21:27:25 -0500

    integrated gluestack and removed CustomButtons usage

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,32 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { router } from 'expo-router'
-import CustomButton from "../../Frontend/components/CustomButton";
-import AsyncStorage from '@react-native-async-storage/async-storage'
+import { Button, ButtonText, ButtonSpinner, ButtonIcon } from '../../components/ui/button';
+
+
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { router } from 'expo-router';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
 
 const Home = () => {
-    return (
-        <View className="bg-primary h-full">
-            <Text>Home</Text>
+    return <View className="bg-primary h-full">
+        <Text>Home</Text>
 
-            <View className="flex-1 justify-center items-center">
-                <CustomButton
-                    title="Logout"
-                    handlePress={() => {
-                        AsyncStorage.setItem('isLoggedIn', JSON.stringify(false));
-                        router.back();
-                        router.back();
-                    }}
-                    containerStyles="w-40 h-20 justify-center items-center bg-red-500"
-                />
-            </View>
+        <View className="flex-1 justify-center items-center">
+            <Button action={"primary"} variant={"solid"} size={"lg"} isDisabled={false}
+                className="w-40 h-20 justify-center items-center bg-red-500"
+                onPress={() => {
+                    AsyncStorage.setItem('isLoggedIn', JSON.stringify(false));
+                    router.push('/(auth)/sign_in')
+                }}>
+                <ButtonText>Log out</ButtonText>
+            </Button>
+
+
 
         </View>
 
-
-
-    )
-}
-
-export default Home
-
+    </View>;
+};
+export default Home;
--- app/index.tsx
+++ app/index.tsx
@@ -1,57 +1,63 @@
+import { Button, ButtonText, ButtonSpinner, ButtonIcon } from '../components/ui/button';
+import React from 'react';
 import { ScrollView, Text, View, Image } from "react-native";
-import { Redirect, router } from 'expo-router';
-import "../global.css"
+import { Link, Redirect, router } from 'expo-router';
+import "../global.css";
 import { SafeAreaView } from "react-native-safe-area-context";
-import CustomButton from "../Frontend/components/CustomButton";
-import AsyncStorage from '@react-native-async-storage/async-storage'
-import { useState, useEffect } from 'react'
 
+import { LogBox } from 'react-native';
 
+LogBox.ignoreLogs([
+  'Text strings must be rendered within a <Text> component'
+]);
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useState, useEffect } from 'react';
+import { GluestackUIProvider } from '../components/ui/gluestack-ui-provider';
+import { opacity } from 'react-native-reanimated/lib/typescript/Colors';
 export default function Index() {
+  const [value, setValue] = React.useState(30);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   async function getData() {
     const data = await AsyncStorage.getItem('isLoggedIn');
-    console.log(data, 'at index.tsx')
+    console.log(data, 'at index.tsx');
     setIsLoggedIn(data == 'true');
   }
-
   useEffect(() => {
     getData();
   }, []);
-  return (
-    <SafeAreaView className="bg-primary h-full">
+  return <SafeAreaView className="bg-primary h-full">
 
-      <ScrollView contentContainerStyle={{ height: '100%' }}>
-        <View className=" w-full min-h-[85vh] justify-center items-center  px-4">
-          <Image
-            source={require('../Frontend/assets/images/logo.png')}
-            className="w-52 h-52"
-            resizeMode="contain"
-          />
+    <ScrollView contentContainerStyle={{
+      height: '100%'
+    }}>
 
-          <Text className="mt-5 px-5 font-custom text-5xl leading-relaxed color-light" >
-            D
-            <Text className="color-secondary font-custom">
-              ream
-            </Text>
-            S
-            <Text className="color-secondary font-custom">
-              cope
-            </Text>
+      <View className=" w-full min-h-[85vh] justify-center items-center  px-4">
+
+        <Image source={require('../Frontend/assets/images/logo.png')} className="w-52 h-52" resizeMode="contain" />
+
+        <Text className="mt-5 px-5 font-custom text-5xl leading-relaxed color-light">
+          D
+          <Text className="color-secondary font-custom">
+            ream
           </Text>
+          S
+          <Text className="color-secondary font-custom">
+            cope
+          </Text>
+        </Text>
 
-          <CustomButton
-            title="Proceed app!"
-            handlePress={() => {
-              isLoggedIn ? router.push('/(tabs)/home') : router.push('/(auth)/sign_in');
-            }
 
-            }
-          />
+        <Button action={"primary"} variant={"link"} size={"lg"} isDisabled={false}
+          className='p-3 bg-light rounded-3xl top-5 active:opacity-75'
+          onPress={() => isLoggedIn ? router.push('/(tabs)/home') : router.push('/(auth)/sign_in')}
+        >
+          <ButtonText>Proceed to app</ButtonText>
+        </Button>
 
-        </View>
-      </ScrollView>
 
-    </SafeAreaView>
-  );
+      </View>
+    </ScrollView>
+
+  </SafeAreaView>;
 }
